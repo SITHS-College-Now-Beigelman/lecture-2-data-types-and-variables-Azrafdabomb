d9cec8ebabc8c2506bf2e85e395d2e3547fe837f
FloppyDuck <158211952+Azrafdabomb@users.noreply.github.com>
2024-09-16 16:28:43 -0400

    Azraf Hossain Lecture 2

---
+++ Hossain_AzrafLecture2.cpp
@@ -0,0 +1,47 @@
+// Azraf Hossain
+// Lecture 2
+// 9/16/24
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    // define variables
+    string words = "are you sigma?";
+    int add = 4;
+    float flt = 4.1;
+    bool yes = true;
+    char letter = 'S';
+    double numbers = 4.52836; 
+
+    double equations = 2 + 3.5;
+    double equationss = 6 / 4 + 3.9;
+    double equationsss = 5.4 * 2 - (13.6 + 18 / 2);
+
+
+    // output all datatypes and equations
+    cout << words << endl;
+    cout << add << endl;
+    cout << flt << endl;
+    cout << yes << endl;
+    cout << letter << endl;
+    cout << numbers << endl;  
+
+
+    cout << "2 + 3.5" << endl;
+    cout << "6 / 4 + 3.9" << endl;
+    cout << "5.4 * 2 - (13.6 + 18 / 2)" << endl;
+    cout << equations << endl;
+    cout << equationss << endl;
+    cout << equationsss << endl;
+
+    // define characters
+    char one = "A";
+
+    return 0;
+
+}
