e916acf54152ffe267a0b9f01443ea0855f4ffd9
FloppyDuck <158211952+Azrafdabomb@users.noreply.github.com>
2024-09-17 15:41:15 -0400

    Azraf Hossain 9/16/24 Lab 2

--- Hossain_AzrafLecture2.cpp
+++ Hossain_AzrafLecture2.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(){
 
-    // define variables
+    // define datatypes
     string words = "are you sigma?";
     int add = 4;
     float flt = 4.1;
@@ -18,12 +18,7 @@ int main(){
     char letter = 'S';
     double numbers = 4.52836; 
 
-    double equations = 2 + 3.5;
-    double equationss = 6 / 4 + 3.9;
-    double equationsss = 5.4 * 2 - (13.6 + 18 / 2);
-
-
-    // output all datatypes and equations
+    // output of all datatypes
     cout << words << endl;
     cout << add << endl;
     cout << flt << endl;
@@ -32,6 +27,28 @@ int main(){
     cout << numbers << endl;  
 
 
+    // code from slide 37
+    int inches; //variable to store total inches
+    inches = 100; //store 100 in the variable inches
+
+    cout << inches << " inch(es) = "; //output the value of inches and the equal sign 
+    cout << inches / 12 << " feet (foot) and "; //output maximum number of feet (foot) 
+    cout << inches % 12 << " inch(es)" << endl; //output remaining inches
+    
+
+    // define characters
+    char a = 'A';
+    char b = 'B';
+
+    // output the comparisons
+    cout << (a > b);
+
+
+    // Defination and output of equations
+    double equations = 2 + 3.5;
+    double equationss = 6 / 4 + 3.9;
+    double equationsss = 5.4 * 2 - (13.6 + 18 / 2);
+    
     cout << "2 + 3.5" << endl;
     cout << "6 / 4 + 3.9" << endl;
     cout << "5.4 * 2 - (13.6 + 18 / 2)" << endl;
@@ -39,8 +56,6 @@ int main(){
     cout << equationss << endl;
     cout << equationsss << endl;
 
-    // define characters
-    char one = "A";
 
     return 0;
 
